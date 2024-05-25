3036eb46c9f1b8ed0be22eec0891839a5220a999
Nathan Santos <nathanansantos@gmail.com>
2024-05-25 14:07:59 -0300

    Feito os primeiros controllers e views

---
+++ CadastroDeEnderecos/Controllers/EnderecoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroDeEnderecos.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
